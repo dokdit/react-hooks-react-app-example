fde8965bc0a833b1ff5356bfa6646aa85b588828
AK <akueiminyang@cmail.carleton.ca>
2023-05-28 17:24:47 -0400

    Lab completed

---
+++ build/precache-manifest.17396c1f7551cfbd542d9a1ab425b15d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "186d35b77a864c5ad1f1",
+    "url": "/static/css/main.9b8521db.chunk.css"
+  },
+  {
+    "revision": "186d35b77a864c5ad1f1",
+    "url": "/static/js/main.186d35b7.chunk.js"
+  },
+  {
+    "revision": "42ec484b6cb52eca3e08",
+    "url": "/static/js/1.42ec484b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c436f3521efbb5a4cba5544a74982e43",
+    "url": "/index.html"
+  }
+];
